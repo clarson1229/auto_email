3f0831f4bf863960bebe7fa6adf1be96a2f7e76f
Connor <Clarson1229@live.com>
2020-10-26 12:06:34 -0600

    pleaseWokr

--- src/utils/sendGrid.js
+++ src/utils/sendGrid.js
@@ -2,12 +2,13 @@
 //a) set sendgrid's api key
 const sgMail = require('@sendgrid/mail');
 const createHtmlEmail = require('./buildmail');
+
 sgMail.setApiKey(process.env.GATSBY_SENDGRID_KEY); 
 
 module.exports = (message) => {
     return sgMail.send({ 
-            to: process.env.MY_EMAIL,
-            from: process.env.MY_EMAIL,
+            to: process.env.GATSBY_MY_EMAIL,
+            from: process.env.GATSBY_MY_EMAIL,
             subject: 'Hock-Hocking-report',
             html: createHtmlEmail(message), //b) create html email & send
             });
