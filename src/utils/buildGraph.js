1305b04c31e2ba6db65543723af3e232aeb2ca1e
Connor <Clarson1229@live.com>
2020-11-08 16:23:40 -0700

    more_tests

--- src/utils/buildGraph.js
+++ src/utils/buildGraph.js
@@ -4,23 +4,24 @@ module.exports = async (data) => {
     const myChart = new QuickChart();
     const dwrdataMeasures = data.flowRate.map(item=> {return (item.measure)});
     const dwrdataLabels = data.flowRate.map(item=> {return (item.dateTime)});
+    console.log('labels', dwrdataLabels)
+    console.log('measures', dwrdataMeasures)
+    
     const chartData= {
-        labels: dwrdataLabels, 
+        labels: ['1','2','3','4'], 
         datasets: [
             {
                 label: `Hocking Discharge in `,
-                data: dwrdataMeasures,
-                backgroundColor: '#0000ff', // color of points 
-                borderColor: '#0000ff',   // color of line
-                borderWidth: 2, 
-                fill: false,
-                pointRadius: 0, 
-                pointHoverRadius: 5,
-                steppedLine: true,
-                fontSize: '2',
-                yAxisID: 'discharge_point',
-            }
-        ]
+                data: [2,4,6,8],
+                // backgroundColor: '#0000ff', // color of points 
+                // borderColor: '#0000ff',   // color of line
+                // borderWidth: 2, 
+                // fill: false,
+                // pointRadius: 0, 
+                // steppedLine: true,
+                // fontSize: '2',
+                // yAxisID: 'discharge_point',
+            }]
     }
     // ${dData.flowRateUnits}
     myChart.setConfig({
