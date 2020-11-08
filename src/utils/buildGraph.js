bd351eed1c466614cf72505047d1ad048e9d4690
Connor <Clarson1229@live.com>
2020-11-08 13:23:29 -0700

    testing

--- src/utils/buildGraph.js
+++ src/utils/buildGraph.js
@@ -1,8 +1,24 @@
-const Chart = require('chart.js');
+const ChartJS = require('chart.js');
+const { CanvasRenderService } = require('chartjs-node-canvas');
+const width = 400;
+const height = 400;
+const chartCallback = (ChartJS) => {
+
+    // Global config example: https://www.chartjs.org/docs/latest/configuration/
+    ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
+    // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
+    ChartJS.plugins.register({
+        // plugin implementation
+    });
+    // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
+    ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
+        // chart implementation
+    });
+};
+const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
 
 module.exports = async (data) => { 
-    const canvas = new HTMLCanvasElement();
-    var myChart = new Chart(canvas, {
+    const configuration = {
         type: 'bar',
         data: {
             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -18,7 +34,7 @@ module.exports = async (data) => {
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
+                    'rgba(255,99,132,1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
@@ -30,13 +46,20 @@ module.exports = async (data) => {
         },
         options: {
             scales: {
-                y: {
-                    beginAtZero: true
-                }
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        callback: (value) => '$' + value
+                    }
+                }]
             }
         }
-    });
+    };
+    const image = await canvasRenderService.renderToBuffer(configuration);
+    const dataUrl = await canvasRenderService.renderToDataURL(configuration);
+    const stream = canvasRenderService.renderToStream(configuration);
+    
 
     
-    return myChart.toBase64Image();
+    return dataUrl;
 }
