2c46ccf8ff91b63bd687aeb6bbbcfb99a5019831
Connor <Clarson1229@live.com>
2020-11-08 17:28:15 -0700

    dataisthere

--- src/utils/buildGraph.js
+++ src/utils/buildGraph.js
@@ -24,63 +24,7 @@ module.exports = async (data) => {
             }]
     }
     // ${dData.flowRateUnits}
-    // myChart.setConfig({
-    //     type: 'line',
-    //     data: chartData, 
-    //     options: {
-    //         scales: {
-                // yAxes: [
-                //     {   
-                //         id: 'discharge_point',
-                //         type: 'linear',
-                //         scaleLabel:{
-                //             display:true,
-                //             labelString: 'CFS',
-                //             // fullDWRdata.flowRateUnits,
-                //             fontColor: '#0000ff',
-                //             fontSize: '14',
-                //             padding: '8',
-                //         },
-                //         ticks: {
-                //             // stepSize: .05,
-                //             fontSize: '14',
-                //             fontColor: '#0000ff'
-                //         }, 
-                //         gridLines: {
-                //             display: false
-                //         }, 
-                //     }
-                // ], 
-    //             xAxes:[
-    //                 {
-    //                     type: 'time',
-    //                     time: {
-    //                         unit: 'day',
-    //                         distribution: 'series'
-    //                     },
-    //                     gridLines: {
-    //                         display: false
-    //                     },
-    //                     ticks: {
-    //                         fontSize: '15',
-    //                     }, 
-    //                     scaleLabel:{
-    //                         display:true,
-    //                         labelString: 'Date',
-    //                         fontSize: '16',
-    //                         fontColor: '#000'
-    //                     }
-    //                 }
-    //             ]
-    //         }}
-    // }).setWidth(2000).setHeight(600);
-
-    // You can send the URL to someone...
-    // const chartImageUrl = myChart.getUrl();
-    tempLabel
-    tempMeasure
-
-    const gConfig={
+    myChart.setConfig({
         "type": "line", 
         "data": {
             "labels":tempLabel, 
@@ -108,7 +52,10 @@ module.exports = async (data) => {
                 }]
             }
         }
-    }
-    console.log(gConfig)
-    return `https://quickchart.io/chart?c=${gConfig}`;
+    }).setWidth(4200).setHeight(600);
+
+    // You can send the URL to someone...
+    const chartImageUrl = myChart.getUrl();
+
+    return chartImageUrl;
 }
